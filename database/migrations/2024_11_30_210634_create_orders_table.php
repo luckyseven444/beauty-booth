b8f7814d15cd9d244fca3d246b984ddfb45f6717
Gazi Salah Uddin <gazisalahuddin73@gmail.com>
2024-12-01 03:10:16 +0600

    migration files created

---
+++ database/migrations/2024_11_30_210634_create_orders_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('grand_total', 10, 2);
+            $table->decimal('shipping_cost', 10, 2)->default(0);
+            $table->decimal('discount', 10, 2)->default(0);
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
