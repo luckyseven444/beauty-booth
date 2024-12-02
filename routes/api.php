da15980770c52b18a6eec15486bb3c420c19278a
Gazi Salah Uddin <gazisalahuddin73@gmail.com>
2024-12-02 08:24:09 +0600

    api.php refactored

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,7 @@
 <?php
 
-// use Illuminate\Http\Request;
-// use Illuminate\Support\Facades\Route;
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
-
-Route::get('/', function () {
-    return response()->json([
-        'success' => true,
-        'message' => 'Transaction created successfully',
-        'data' => 55, // Return the created customer object
-    ], 200);
-});
 
 // Group routes under a common prefix
 Route::prefix('category/{slug}/products')->group(function () {
